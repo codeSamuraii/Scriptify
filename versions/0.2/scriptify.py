7c0473c3d9f96cae8081665ba7dde9e5e4aae367
codeSamuraii <remi@heneault.fr>
2018-05-09 20:12:59 +0200

    started new functionnalities

--- versions/0.2/scriptify.py
+++ versions/0.2/scriptify.py
@@ -4,6 +4,7 @@ Scriptify v0.2
 © Rémi Héneault (@codeSamuraii)
 https://github.com/codeSamuraii
 """
+import base64 as base64enc
 from os import path
 from argparse import ArgumentParser, FileType
 
@@ -13,7 +14,7 @@ def get_arguments():
         prog="scriptify.py",
         description="Turn any file into a runnable python script",
         usage=("scriptify.py [-h] [--help]\n       "
-               "scriptify.py 'source file' 'output file' [-m] [-b] [-c [-p pass]] [-s msg]"),
+               "scriptify.py 'source file' 'output file' [-m] [-b | -c [-p pass]] [-s msg]"),
         epilog="If encryption is used and no password is set, the key will be stored in the script!")
 
     parser.add_argument('in_file',
@@ -29,10 +30,11 @@ def get_arguments():
     parser.add_argument('-m', '--minimal', action='store_true',
                         help="use a minimal/obfuscated recovery script")
 
-    parser.add_argument('-b', '--base64', action='store_true',
+    encryption = parser.add_mutually_exclusive_group()
+    encryption.add_argument('-b', '--base64', action='store_true',
                         help="encode data with base64")
 
-    parser.add_argument('-c', '--crypted', action='store_true',
+    encryption.add_argument('-c', '--crypted', action='store_true',
                             help="encrypt data with AES")
 
     parser.add_argument('-p', '--pass', metavar='*** ', dest='password',
@@ -65,6 +67,44 @@ def print_welcome(args):
         exit()
 
 
+def file_to_string(file_obj):
+    print("* Reading source file... ", end='')
+    with file_obj as source_file:
+        binary_content = source_file.read()
+
+    hex_string = repr(binary_content)
+    print("OK.")
+    return hex_string
+
+
+def file_to_buffer(file_obj):
+    print("* Reading source file... ", end='')
+    with file_obj as source_file:
+        binary_content = source_file.read()
+
+    print("OK.")
+    return binary_content
+
+
 if __name__ == '__main__':
     args = get_arguments()
     print_welcome(args)
+
+    in_file, out_file = args.in_file, args.out_file
+    raw_buffer = file_to_buffer(in_file)
+
+    if args.base64:
+        print("* Encoding in Base64...", end='')
+        file_buffer = base64enc.b64encode(raw_buffer)
+        print("OK.")
+
+    elif args.crypted:
+        # TODO: ...
+        pass
+
+    else:
+        file_buffer = raw_buffer
+
+    hex_string = repr(file_buffer)
+
+    # COMBAK: To be continued
